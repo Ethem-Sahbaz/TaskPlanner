16f173bc3d033691770b6992908a132e65c40f30
Ethem Sahbaz <ethemsahbaz@outlook.com>
2025-01-03 13:28:22 +0100

    mostly finished ui

--- src/TaskPlanner.WPF/App.xaml.cs
+++ src/TaskPlanner.WPF/App.xaml.cs
@@ -6,5 +6,13 @@ namespace TaskPlanner.WPF;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainWindow = new MainWindow();
+        
+        MainWindow.Show();
+        
+        base.OnStartup(e);
+    }
 }
 
---
+++ src/TaskPlanner.WPF/MainViewModel.cs
@@ -0,0 +1,6 @@
+namespace TaskPlanner.WPF;
+
+public class MainViewModel
+{
+    
+}
